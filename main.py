ded04f0129d3941607f94163f9b79c115a7bdf36
deigol <deigolsalazar@gmail.com>
2024-11-09 18:21:16 -0700

    Added frame to introduce text

--- main.py
+++ main.py
@@ -52,4 +52,30 @@ boton_sintactico.grid(row=0, column=2, padx=15)
 boton_completo = tk.Button(frame_botones, text="Análisis Completo", width=15, height=5)
 boton_completo.grid(row=0, column=3, padx=15)
 
+#Ingreso de texto
+# Frame para entrada de texto y conteo de líneas con scroll
+frame = tk.Frame(ventana)
+frame.place(relwidth=0.80, relheight=0.25, relx=0.5, rely=0.30, anchor="center")
+
+# Scrollbar
+scrollbar = tk.Scrollbar(frame)
+scrollbar.pack(side="right", fill="y")
+
+# Widget de conteo de líneas
+lineas = tk.Text(frame, width=4, height=20, bg="#333333", fg="white", font=("Courier", 14), state="disabled")
+lineas.pack(side="left", fill="y")
+
+# Configuración de entrada de texto
+entrada = tk.Text(frame, wrap="word", width=200, height=20, bg="#c9c9c9", fg="black", insertbackground="white",
+                  font=("Courier", 14), yscrollcommand=scrollbar.set)
+entrada.tag_configure("palabraReservada", foreground="blue")
+entrada.tag_configure("operador", foreground="red")
+entrada.tag_configure("numero", foreground="purple")
+entrada.tag_configure("identificador", foreground="black")
+entrada.tag_configure("invalido", foreground="black", underline=True)
+entrada.pack(side="left", fill="both", expand=True)
+
+# Configurar scrollbar para entrada y sincronizar con líneas
+scrollbar.config(command=entrada.yview)
+
 ventana.mainloop()
