66cd8c56cab60081ce63be5d2357db7947847f75
deigol <deigolsalazar@gmail.com>
2024-11-09 18:59:51 -0700

    Created table to show results from analysis

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ ventana.title("Analizador Léxico y Sintáctico")
 ventana.geometry("1280x900")
 ventana.config(bg= "PINK");
 
-#Botones
+#BOTONES
 frame_botones = tk.Frame(ventana, bg='lightblue')
 frame_botones.pack(pady=15)
 
@@ -52,7 +52,7 @@ boton_sintactico.grid(row=0, column=2, padx=15)
 boton_completo = tk.Button(frame_botones, text="Análisis Completo", width=15, height=5)
 boton_completo.grid(row=0, column=3, padx=15)
 
-#Ingreso de texto
+#TEXTO
 # Frame para entrada de texto y conteo de líneas con scroll
 frame = tk.Frame(ventana)
 frame.place(relwidth=0.80, relheight=0.25, relx=0.5, rely=0.30, anchor="center")
@@ -66,7 +66,7 @@ lineas = tk.Text(frame, width=4, height=20, bg="#333333", fg="white", font=("Cou
 lineas.pack(side="left", fill="y")
 
 # Configuración de entrada de texto
-entrada = tk.Text(frame, wrap="word", width=200, height=20, bg="#c9c9c9", fg="black", insertbackground="white",
+entrada = tk.Text(frame, wrap="word", width=200, height=20, bg="#c9c9c9", fg="black", insertbackground="PURPLE",
                   font=("Courier", 14), yscrollcommand=scrollbar.set)
 entrada.tag_configure("palabraReservada", foreground="blue")
 entrada.tag_configure("operador", foreground="red")
@@ -77,5 +77,34 @@ entrada.pack(side="left", fill="both", expand=True)
 
 # Configurar scrollbar para entrada y sincronizar con líneas
 scrollbar.config(command=entrada.yview)
+
+#TABLA
+frame_tabla = tk.Frame(ventana)
+frame_tabla.pack(pady=30, fill="x", expand=True)
+
+# Tabla para mostrar los resultados
+# Crear un frame contenedor para la tabla
+frame_tabla = tk.Frame(ventana,  bg="BLACK")
+frame_tabla.pack(pady=10, fill="x", expand=False)  # Se usa pack para la posición
+
+# Tabla para mostrar los resultados
+columnas = ("columna", "lexema", "token", "linea", "posicion")
+tabla = ttk.Treeview(frame_tabla, columns=columnas, show="headings", height=10)  # Reducir altura a 10 filas
+
+# Configurar encabezados
+tabla.heading("columna", text="Columna")
+tabla.heading("lexema", text="Lexema")
+tabla.heading("token", text="Token")
+tabla.heading("linea", text="Línea")
+tabla.heading("posicion", text="Posición")
+
+# Usar pack en el frame_tabla con un tamaño ajustado
+tabla.pack(pady=5, fill="x", expand=False)
+
+# Ajustar el tamaño del frame_tabla para que ocupe un 80% del ancho de la ventana
+frame_tabla.config(width=int(ventana.winfo_width() * 0.8))  # 80% del ancho de la ventana
+
+# Para centrar el frame_tabla respecto a la ventana
+frame_tabla.place(relx=0.5, rely=0.60, anchor="center")  # Centrado respecto a la ventana
 
 ventana.mainloop()
